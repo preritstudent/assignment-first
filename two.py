4cfc9e06cc34d226e077538d6ceb2e8b0fcb50e1
preritstudent <prerit.student@gmail.com>
2024-05-14 15:01:27 +0530

    sender file upload

---
+++ two.py
@@ -0,0 +1,48 @@
+# print("hello")
+
+# var1=10
+# var2=25,21
+# var3=True 
+# var4="Upflair"
+# var5="jecrc"
+# var6="""jecrc foundation"""
+# print(var1)
+# print(var2)
+# print(var3)
+# print(var4)
+# print(var5)
+# print(var6)
+
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(type(var4))
+# print(type(var5))
+# print(type(var6))
+
+
+# # rupee=25000
+# # print("i have =",rupee,"rupee")
+# paragraph="""hjdsmdvndsvldsvnldsnldnvldsnndsnlkvndslvndsvnldsvldsnvlkdsnlvkdslkdsdvkldsvlkdsvdsndskndskldsvklds
+# vncjkldsvldsvdsvdsvndsnvkds
+# dsnckdsbvkjdsd
+# """
+# print(paragraph)
+# college="jecrc foundation"
+# print(college.upper())
+# print(college.lower())
+# # print(len(college))
+# # print(college.count('c'))
+# # print(college.endswith('n'))
+# print(college.endswith('f'))
+# college="jecrc foundation"
+# print(college.strip())
+# # print(college.lstrip())
+# print(college.rstrip())
+# print(college[0:3])
+
+college="jecrc JAIPUR rajasthan"
+# print(college[::-1])
+# print(college[4::-1])
+# print(college[17:])
+print(college[22::-1])
