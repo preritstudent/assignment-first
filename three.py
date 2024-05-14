98d2ce0dd46923ceb9c333aea605931c57b7fef0
preritstudent <prerit.student@gmail.com>
2024-05-14 14:09:27 +0530

    day 3 code uploaded

---
+++ three.py
@@ -0,0 +1,66 @@
+# lst=[24,41,34,56,'hello',True,34,45]
+# # print (lst(1:))
+# # lst[1]=500
+# # print(len(lst))
+# print(lst)
+# lst.pop(5)
+# lst.insert(5,'0')
+# lst.pop()
+# lst.append(98)
+# lst.append(99)
+
+# print(lst)
+# lst=[24,41,34,56,'hello',True,34,45]
+# print(lst[4][-1])
+
+# lst=[25,42,63,25,[100,200,300]]
+# print(lst[4][1])
+# lst[1]=1000
+# print(lst)
+# tpl=(10,20,30,40,75.25,'jecrc',True)
+# tpl[1]=200
+# print(tpl)
+
+# print(tpl[1])
+# # print(tpl)
+# print(type(tpl))
+# name="jecrc"
+# college_name=('jecrc',)
+# print(type(college_name))
+# college_name=("jecrc","skit","amity")
+# college="jecrc",
+# print(type(college_name))
+# print(college)
+# lst=[12,34,25,63,85,95]
+# print(lst)
+# lst.remove(85)
+# print(lst)
+
+# name="jecrc"
+# surname=" foundation"
+# roll_no=101
+
+# # print(name*56)
+# print(name +surname)
+# print(name*56)
+# print(name+ roll_no)
+# tpl2=(10,20,30,40)
+# print(tpl2*8)
+
+# lst1=[10,20,30,40]
+# lst2=[11,22,33,44]
+# # lst1.append(lst2)
+# lst1.extend(lst2)
+# print(lst1)
+
+#key:values
+# dtt ={"name":"rohit","branch":'cse',"rollno":199}
+# # print(dtt)
+# # print(type(dtt))
+# # print(dtt["rollno"])
+# print(dtt.keys())
+# print(dtt.values())
+# dtt ={"0":"rohit","1":'cse',"2":199}
+# print(dtt)
+class_data={"name":["a","b","d"],"marks":[2,3,6,7]}
+print(class_data["marks"])
